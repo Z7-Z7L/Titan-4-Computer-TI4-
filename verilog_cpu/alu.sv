533aae7d2d6e6976c32c4332d2ebb7ff5778da0a
Z7-Z7L <zenitsu998@gmail.com>
2025-08-06 23:18:56 +0300

    alu update 0.001

--- verilog_cpu/alu.sv
+++ verilog_cpu/alu.sv
@@ -1,25 +1,25 @@
 // ALU
 module ALU(o, flag, opcode,a,b);
-  output [3:0] o;  
+  output reg [3:0] o;  
   output [7:0] flag;
 
   input [3:0] opcode, a,b;
 
-  case(opcode)
-    4'b0001; // ADD
-    4'b0010; // SUB
-    4'b0011; // AND 
-    4'b0100; // ORR
-    4'b0101; // NOR
-    4'b0110; // XOR
-    4'b0111; // RSH
-    4'b1000; // LSH
-    4'b1001; // LDI
-    4'b1010; // ADI
-    4'b1101; // CMP
-
-
-
-  endcase;
+  always @(*) begin
+    case(opcode)
+      4'b0001: o = a + b; // ADD
+      4'b0010: o = a - b; // SUB
+      4'b0011: o = a & b; // AND 
+      4'b0100: o = a | b; // ORR
+      4'b0101: o = ~(a | b); // NOR
+      4'b0110: o = a ^ b; // XOR
+      4'b0111: o = a >> 1; // RSH
+      4'b1000: o = a << 1; // LSH
+      4'b1001: o = b; // LDI
+      4'b1010: a = a + b; // ADI
+      // 4'b1101: ; // CMP
+      default: o = 4'b0000;
+    endcase
+  end
 
 endmodule
