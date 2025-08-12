8f9d27672c64fd672b21f53c7347a406ff242e52
Z7-Z7L <zenitsu998@gmail.com>
2025-08-12 23:54:23 +0300

    .

--- verilog_cpu/mem.sv
+++ verilog_cpu/mem.sv
@@ -1,5 +1,25 @@
-module Mem (
-  
+module Mem #(
+  parameter ADDR_WIDTH = 5, // 5-bit address
+  parameter DATA_WIDTH = 4, // 4-bit data
+  parameter MEM_SIZE = 1 << ADDR_WIDTH
+) (
+  output reg [DATA_WIDTH-1:0] data_out,
+
+  input wire clk,
+  input wire we, // write enable
+  input wire [ADDR_WIDTH-1:0] addr, // address
+  input wire [DATA_WIDTH-1:0] data_in  
 );
+
+  // memory array
+  reg [DATA_WIDTH-1:0] memory [0:MEM_SIZE-1];
+
+  always @(posedge clk) begin
+    if (we) begin
+      memory[addr] <= data_in; // Write
+    end
+    data_out <= memory[addr];    // Read
+  end
+
   
 endmodule
